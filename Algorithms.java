604eeca03042aaa20975e2f9e448e898c2691e52
Jennifer Be <jbe@berkeley.edu>
2016-02-05 22:44:39 -0800

    set up computeDepth

--- Algorithms.java
+++ Algorithms.java
@@ -23,7 +23,7 @@ public class Algorithms {
 	 * or number that is higher than it's neighbors (it can be on the edge)
 	 * in O(log n) times, where n = the number of items in the list.
 	 *
-	 * @param elements
+	 * @param elements is a list of unique numbers
 	 * @return the index of the "peak" number
 	 */
 	public Number findPeak(ArrayList<Number> elements) {
@@ -44,4 +44,32 @@ public class Algorithms {
 		}
 	}
 
+	/**
+	 * Compute the Depth of All Vertices
+	 * @computeDepth takes in a tree of a graph and its root, and returns an ArrayList
+	 * of depths in O(|V| + |E|), or O(n), time by depth first search.
+	 * Depth, defined by the problem, starts at 0 (at the root) and increments by one
+	 * at each level. If a node has been visited, it's depth is its initial value.
+	 *
+	 * @param t is the tree with numbered vertices for which we are finding depths.
+	 * @return an ArrayList with the depths in the corresponding index.
+	 */
+	public ArrayList computeDepth(TreeNode tree) {
+		ArrayList<Integer> depths = new ArrayList();
+		Stack<TreeNode> fringe = new Stack(tree);
+		int depth = 0;
+
+
+	}
+
+	public int computeDepthHelper(TreeNode tree, int depth)
 }
+
+
+
+
+
+
+
+
+
