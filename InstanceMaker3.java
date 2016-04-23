4eeb459a351b730dae63ac55d80aa70bca5959c9
Jennifer Be <jbe@berkeley.edu>
2016-04-23 19:05:47 -0400

    added matrix makers

---
+++ InstanceMaker3.java
@@ -0,0 +1,132 @@
+import java.util.ArrayList;
+import java.io.*;
+import java.util.Collections;
+import java.util.*;
+
+public class InstanceMaker3{
+	static PrintWriter writer;
+	static int[][] graphVertices;
+	static ArrayList<Integer> randomVertex;
+	static int row, col, vcount;
+	static HashMap<Integer, ArrayList<Integer>> dict;
+
+	public static void initializer(int row, int col) {	
+		vcount = row * col;
+		writer.println(vcount);
+		writer.println("");
+		randomVertex = new ArrayList();
+		for (int i = 0; i < vcount; i++) {
+			randomVertex.add(i);
+		}
+		Collections.shuffle(randomVertex);
+
+		graphVertices = new int[row][];
+		ListIterator<Integer> iterator = randomVertex.listIterator();
+		for (int i = 0; i < row; i++) {
+			int[] newColumn = new int[col];
+
+			for (int j = 0; j < col; j++) {
+				int index = (i*col) + j;
+				newColumn[j] = iterator.next();
+			}
+			graphVertices[i] = newColumn;
+			System.out.println(graphVertices[i][0]);
+		}
+
+		dict = new HashMap<Integer, ArrayList<Integer>>();
+		ArrayList stuff;
+		// column edges
+		for (int i = 0; i < row; i += 1) {
+			if (i % 2 == 0) {
+				for (int j = 0; j < col; j += 2) {
+					stuff = new ArrayList();
+					if (i != 0) {
+						stuff.add(graphVertices[i - 1][j]);
+					} else {
+						stuff.add(graphVertices[row - 1][j]);
+					}
+					if (i != row - 1) {
+						stuff.add(graphVertices[i + 1][j]);
+					} else {
+						stuff.add(graphVertices[0][j]);
+					}
+					dict.put(graphVertices[i][j], stuff);
+				}
+			} else {
+				for (int j = 1; j < col; j += 2) {
+					stuff = new ArrayList();
+					stuff.add(graphVertices[i - 1][j]);
+					if (i != row - 1) {
+						stuff.add(graphVertices[i + 1][j]);
+					} else {
+						stuff.add(graphVertices[0][j]);
+					}
+					dict.put(graphVertices[i][j], stuff);
+				}
+			}
+		}
+
+		for (int j = 0; j < col; j += 1) {
+			if (j % 2 == 0) {
+				for (int i = 1; i < row; i += 2) {
+					stuff = new ArrayList();
+					if (j != 0) {
+						stuff.add(graphVertices[i][j - 1]);
+					} else {
+						stuff.add(graphVertices[i][col - 1]);
+					}
+					if (j != col - 1) {
+						stuff.add(graphVertices[i][j + 1]);
+					} else {
+						stuff.add(graphVertices[i][0]);
+					}
+					dict.put(graphVertices[i][j], stuff);
+				}
+			} else {
+				for (int i = 0; i < row; i += 2) {
+					stuff = new ArrayList();
+					stuff.add(graphVertices[i][j - 1]);
+					if (j != col - 1) {
+						stuff.add(graphVertices[i][j + 1]);
+					} else {
+						stuff.add(graphVertices[i][0]);
+					}
+					dict.put(graphVertices[i][j], stuff);
+				}
+			}
+		}
+		System.out.println(dict.entrySet());
+	}
+
+	public static void matrixfy() {	
+		for (int i = 0; i < vcount; i++) {
+			String matrix = "";
+			for (int j = 0; j < vcount; j++) {
+				if (dict.get(i).contains(j)) {
+					matrix += "1";
+				} else {
+					matrix += "0";
+				}
+				if (j != vcount - 1) {
+					matrix += " ";
+				}
+			}
+			//System.out.println(matrix);
+			writer.println(matrix);
+		}
+	}
+
+	public static void main(String[] args) {
+		File file = new File("CYCLECONNECTEDALLSIDES.in");
+    	try {
+        writer = new PrintWriter(file);
+        InstanceMaker3.initializer(22, 22);
+        InstanceMaker3.matrixfy();
+        writer.close();       
+    	}
+    	catch (FileNotFoundException ex)  {
+
+        // insert code to run when exception occurs
+    	}
+	}
+}
